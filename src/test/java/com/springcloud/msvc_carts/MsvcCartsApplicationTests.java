c6f85f6f5e66f8759f39244a3b73b026a0f99a04
Castañeda <juxnpxblojpcd315@gmail.com>
2025-11-09 21:39:37 -0500

    ajuste a test

--- src/test/java/com/springcloud/msvc_carts/MsvcCartsApplicationTests.java
+++ src/test/java/com/springcloud/msvc_carts/MsvcCartsApplicationTests.java
@@ -1,18 +1,27 @@
 package com.springcloud.msvc_carts;
 
+import com.springcloud.msvc_carts.infrastructure.integration.client.OrderFeignClient;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import com.springcloud.msvc_carts.infrastructure.integration.client.OrderFeignClient;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootTest(classes = MsvcCartsApplication.class)
+@SpringBootTest(classes = {MsvcCartsApplication.class, MsvcCartsApplicationTests.MockConfig.class})
 @AutoConfigureMockMvc(addFilters = false)
 class MsvcCartsApplicationTests {
 
-	@MockitoBean
-	private OrderFeignClient orderFeignClient;
+	@TestConfiguration
+	static class MockConfig {
+		@Mock
+		private OrderFeignClient orderFeignClient;
 
+		@Bean
+		public OrderFeignClient orderFeignClient() {
+			return orderFeignClient;
+		}
+	}
 
 	@Test
 	void contextLoads() {
