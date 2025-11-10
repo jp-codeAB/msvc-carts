9f896179b427a37c7ed3d4412f4fb90f30416a7f
Castañeda <juxnpxblojpcd315@gmail.com>
2025-11-09 21:53:06 -0500

    ajuste a test

--- src/test/java/com/springcloud/msvc_carts/MsvcCartsApplicationTests.java
+++ src/test/java/com/springcloud/msvc_carts/MsvcCartsApplicationTests.java
@@ -1,27 +1,12 @@
 package com.springcloud.msvc_carts;
 
-
-import com.springcloud.msvc_carts.infrastructure.integration.client.OrderFeignClient;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-@SpringBootTest(classes = {MsvcCartsApplication.class, MsvcCartsApplicationTests.MockConfig.class})
-@AutoConfigureMockMvc(addFilters = false)
 class MsvcCartsApplicationTests {
-
-	@TestConfiguration
-	static class MockConfig {
-		@Bean
-		public OrderFeignClient orderFeignClient() {
-			return Mockito.mock(OrderFeignClient.class);
-		}
-	}
 
 	@Test
 	void contextLoads() {
+		assertDoesNotThrow(() -> MsvcCartsApplication.main(new String[] {}));
 	}
 }
