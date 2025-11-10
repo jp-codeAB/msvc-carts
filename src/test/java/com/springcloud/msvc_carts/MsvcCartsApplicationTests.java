65300467bebed3a09b020c6864c2d426a9d04f07
Castañeda <juxnpxblojpcd315@gmail.com>
2025-11-09 20:57:47 -0500

    ajuste a test

--- src/test/java/com/springcloud/msvc_carts/MsvcCartsApplicationTests.java
+++ src/test/java/com/springcloud/msvc_carts/MsvcCartsApplicationTests.java
@@ -2,12 +2,16 @@ package com.springcloud.msvc_carts;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import com.springcloud.msvc_carts.infrastructure.integration.client.OrderFeignClient;
 
 @SpringBootTest
 class MsvcCartsApplicationTests {
 
+	@MockBean
+	private OrderFeignClient orderFeignClient;
+
 	@Test
 	void contextLoads() {
 	}
-
 }
